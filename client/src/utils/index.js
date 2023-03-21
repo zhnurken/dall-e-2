235ef820bd8c558bdd7406c403e9407095ce31a0
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-21 17:48:19 +0600

    final commit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,17 +1,15 @@
 import FileSaver from 'file-saver';
-
-import {surpriseMePrompts} from '../constants';
-
+import { surpriseMePrompts } from '../constants';
 
 export function getRandomPrompt(prompt) {
-    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
-    const randomPrompt = surpriseMePrompts[randomIndex];
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
 
-    if(randomPrompt === prompt) return getRandomPrompt(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
-    return randomIndex;
+  return randomPrompt;
 }
 
-export async function downloadImage(_id, photo){
-    FileSaver.saveAs(photo, `download-${_id}.jpg`)
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
