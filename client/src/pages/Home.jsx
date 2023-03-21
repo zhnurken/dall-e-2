dfaaba4d754ffe67ca335d0fcc640c7ea53fdeb5
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-21 13:29:39 +0600

    add card

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,37 @@ const Home = () => {
 
   const [searchText, setSearchText] = useState('');
 
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (response.ok) {
+          const result = await response.json();
+
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+          alert (error)
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, [])
+
+  const habdleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  }
+
   return (
    <section className='max-w-7xl mx-auto'>
       <div>
@@ -48,7 +79,7 @@ const Home = () => {
                   />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
