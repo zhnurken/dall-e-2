bd72c5bf0a6caadb073d0634a5cbe17f3d583249
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-21 19:54:27 +0600

    some edit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: "359416934773726",
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
